08c7f8ebf102f219dc47f7c9c750f0ca22fa01ee
Thomas L. Kjeldsen <tlk@closureconsulting.com>
2019-12-20 01:39:50 +0100

    Move back to upstream deps by Envoy
    SPM issue has been resolved, see https://github.com/envoy/Embassy/issues/86

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/IBM-Swift/SwiftyJSON", from: "17.0.5"),
         .package(url: "https://github.com/andybest/linenoise-swift", from: "0.0.3"),
-        .package(url: "https://github.com/tlk/Ambassador", from: "4.0.6"),
+        .package(url: "https://github.com/Envoy/Ambassador", from: "4.0.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

tlk/beoplay-macos-remote-cli

commit ca3ec66a4c76184e1aa5e648c58e151f8508dfab

Bugfix2: prevent SwiftyJSON from entering a busy loop when receiving invalid data response.

Sources/RemoteCore/RemoteControl.swift
@@ -64,23 +64,29 @@ public class RemoteControl {
         func completionData(data: Data?) {
             var sources = [BeoplaySource]()
 
-            guard data != nil && data!.count > 0 else {
+            guard let jsonData = data, jsonData.count > 0 else {
                 completion(sources)
                 return
             }
 
-            let json = JSON(data: data!)
-            for (_, source):(String, JSON) in json["sources"] {
-                let s = BeoplaySource(
-                    id: source[0].stringValue,
-                    sourceType: source[1]["sourceType"]["type"].stringValue,
-                    category: source[1]["category"].stringValue,
-                    friendlyName: source[1]["friendlyName"].stringValue,
-                    borrowed: source[1]["borrowed"].boolValue,
-                    productJid: source[1]["product"]["jid"].stringValue,
-                    productFriendlyName: source[1]["product"]["friendlyName"].stringValue
+            let json = JSON(data: jsonData)
+
+            guard let list = json["sources"].array else {
+                completion(sources)
+                return
+            }
+
+            for element in list {
+                let source = BeoplaySource(
+                    id: element[0].stringValue,
+                    sourceType: element[1]["sourceType"]["type"].stringValue,
+                    category: element[1]["category"].stringValue,
+                    friendlyName: element[1]["friendlyName"].stringValue,
+                    borrowed: element[1]["borrowed"].boolValue,
+                    productJid: element[1]["product"]["jid"].stringValue,
+                    productFriendlyName: element[1]["product"]["friendlyName"].stringValue
                 )
-                sources.append(s)
+                sources.append(source)
             }
             completion(sources)
         }
@@ -120,12 +126,13 @@ public class RemoteControl {
         var favorites = [(String, String)]()
         request(method: "GET", path: "/BeoContent/radio/netRadioProfile/favoriteList/id=f1/favoriteListStation") { data in
 
-            guard data != nil && data!.count > 0 else {
+            guard let jsonData = data, jsonData.count > 0 else {
                 completion(favorites)
                 return
             }
 
-            let json = JSON(data: data!)
+            let json = JSON(data: jsonData)
+
             guard let list = json["favoriteListStationList"]["favoriteListStation"].array else {
                 completion(favorites)
                 return
@@ -208,12 +215,17 @@ public class RemoteControl {
 
     public func getVolume(_ completion: @escaping (Int?) -> ()) {
         func getVolumeFromJSON(_ data: Data?) -> Int? {
-            guard data != nil && data!.count > 0 else {
+            guard let jsonData = data, jsonData.count > 0 else {
                 return nil
             }
 
-            let json = JSON(data: data!)
-            return Int(json["speaker"]["level"].stringValue)
+            let json = JSON(data: jsonData)
+
+            guard let volume = json["speaker"]["level"].string else {
+                return nil
+            }
+
+            return Int(volume)
         }
 
         func completionData(data: Data?) {

Sources/Emulator/DeviceEmulator.swift
@@ -125,7 +125,7 @@ extension DeviceEmulator {
         return self.ns?.name ?? "$no-name$"
     }
 
-    func addRoutes() {
+    func addRoutes(getDataApiEnabled : Bool = true) {
         // Play, original port: 8080
         router["/BeoZone/Zone/Stream/Play"] = JSONResponse() { environ -> Any in
             self.state = RemoteCore.DeviceState.play
@@ -251,36 +251,44 @@ extension DeviceEmulator {
         }))
 
         // Get controlled sources, original port: 80 <-- heads up
-        router["/api/getData"] = DelayResponse(JSONResponse(handler: { _ -> Any in
-            return [["type": "controlledSources", "controlledSources": [
-                "controlledSources": [
-                    [
-                        "deviceId": "",
-                        "enabled": false,
-                        "sourceId": "linein",
-                        "enabledExternal": true
-                    ],
-                    [
-                        "deviceId": "",
-                        "enabled": true,
-                        "sourceId": "radio",
-                        "enabledExternal": true
-                    ],
-                    [
-                        "deviceId": "",
-                        "enabled": false,
-                        "sourceId": "bluetooth",
-                        "enabledExternal": true
-                    ],
-                    [
-                        "deviceId": "9999.1234567.12345678@products.bang-olufsen.com",
-                        "enabled": true,
-                        "sourceId": "spotify:9999.1234567.12345678@products.bang-olufsen.com",
-                        "enabledExternal": false
+        if (getDataApiEnabled) {
+            router["/api/getData"] = DelayResponse(JSONResponse(handler: { _ -> Any in
+                return [["type": "controlledSources", "controlledSources": [
+                    "controlledSources": [
+                        [
+                            "deviceId": "",
+                            "enabled": false,
+                            "sourceId": "linein",
+                            "enabledExternal": true
+                        ],
+                        [
+                            "deviceId": "",
+                            "enabled": true,
+                            "sourceId": "radio",
+                            "enabledExternal": true
+                        ],
+                        [
+                            "deviceId": "",
+                            "enabled": false,
+                            "sourceId": "bluetooth",
+                            "enabledExternal": true
+                        ],
+                        [
+                            "deviceId": "9999.1234567.12345678@products.bang-olufsen.com",
+                            "enabled": true,
+                            "sourceId": "spotify:9999.1234567.12345678@products.bang-olufsen.com",
+                            "enabledExternal": false
+                        ]
                     ]
-                ]
-            ]]]
-        }))
+                ]]]
+            }))
+
+        } else {
+
+            router["/api/getData"] = DataResponse(statusCode: 500, contentType: "application/json; charset=UTF-8") { environ -> Data in
+                return Data("{\"error\":{\"message\":\"Error: path not whitelisted!\",\"name\":\"error\"}}".utf8)
+            }
+        }
 
         // Get TuneIn favorite stations, original port: 8080
         router["/BeoContent/radio/netRadioProfile/favoriteList/id=f1/favoriteListStation"] = DelayResponse(JSONResponse(handler: { _ -> Any in

Sources/RemoteCore/RemoteAdminControl.swift
@@ -9,13 +9,7 @@ public class RemoteAdminControl {
         URLCache.shared = URLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil)
     }
 
-    private func request(method: String,
-            path: String,
-            query: String? = nil,
-            body: String? = nil, 
-            completionData: ((Data?) -> ())? = nil, 
-            _ completion: (() -> ())? = nil) {
-
+    private func request(method: String, path: String, query: String? = nil, body: String? = nil, completionData: ((Data?) -> ())? = nil) {
         var urlComponents = self.endpoint
         urlComponents.path = path
         urlComponents.query = query
@@ -27,7 +21,6 @@ public class RemoteAdminControl {
 
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             completionData?(data)
-            completion?()
         };
 
         task.resume()
@@ -48,15 +41,20 @@ public class RemoteAdminControl {
         func completionData(data: Data?) {
             var sourceIds = [String]()
 
-            guard data != nil && data!.count > 0 else {
+            guard let jsonData = data, jsonData.count > 0 else {
                 completion(sourceIds)
                 return
             }
 
-            let json = JSON(data: data!)
-            for (_, source):(String, JSON) in json[0]["controlledSources"]["controlledSources"] {
-                if source["enabled"].boolValue {
-                    let id = source["sourceId"].stringValue
+            let json = JSON(data: jsonData)
+
+            guard let list = json[0]["controlledSources"]["controlledSources"].array else {
+                completion(sourceIds)
+                return
+            }
+
+            for source in list {
+                if source["enabled"].boolValue, let id = source["sourceId"].string {
                     sourceIds.append(id)
                 }
             }

commit 5c0c055c90c794b8aa3d75783a396aae4b4ddc20

Bugfix getVolume that was broken in ca3ec66a4c76184e1aa5e648c58e151f8508dfab

Sources/RemoteCore/RemoteControl.swift
@@ -221,11 +221,11 @@ public class RemoteControl {
 
             let json = JSON(data: jsonData)
 
-            guard let volume = json["speaker"]["level"].string else {
+            guard let volume = json["speaker"]["level"].int else {
                 return nil
             }
 
-            return Int(volume)
+            return volume
         }
 
         func completionData(data: Data?) {